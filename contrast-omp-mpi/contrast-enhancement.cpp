e1e5914f7579a572a208ae6001372b068fab7e76
Frank <frank.ingsistema@gmail.com>
2023-12-15 23:27:31 +0100

    init mpi

--- contrast-omp-mpi/contrast-enhancement.cpp
+++ contrast-omp-mpi/contrast-enhancement.cpp
@@ -1,93 +1,135 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <omp.h>
+#include "mpi.h"
 #include "hist-equ.h"
 
-PGM_IMG contrast_enhancement_g(PGM_IMG img_in)
+PGM_IMG contrast_enhancement_g(PGM_IMG_MPI img_in)
 {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     PGM_IMG result;
-    int hist[256];
-    
-    result.w = img_in.w;
-    result.h = img_in.h;
-    result.img = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
-    
-    histogram(hist, img_in.img, img_in.h * img_in.w, 256);
-    histogram_equalization(result.img,img_in.img,hist,result.w*result.h, 256);
+    int local_hist[256] = {0};
+    int global_hist[256] = {0};
+
+    int img_size_g = img_in.width * img_in.height;
+    int remainder_g = img_size_g % size;
+
+    if (rank == 0) {
+        result.img = (unsigned char *)malloc(img_in.width * img_in.height * sizeof(unsigned char));
+        result.w = img_in.width;
+        result.h = img_in.height;
+    } else {
+        result.img = NULL;
+        result.w = 0;
+        result.h = 0;
+    }
+
+    // Calculate local histogram for the segment
+    histogram(local_hist, img_in.segment_in_g, img_in.segment_size_g, 256);
+
+    // Aggregate local histograms into a global histogram
+    MPI_Allreduce(local_hist, global_hist, 256, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    // Apply histogram equalization to the segment
+    unsigned char *enhanced_segment = (unsigned char *)malloc(img_in.segment_size_g * sizeof(unsigned char));
+    histogram_equalization(enhanced_segment, img_in.segment_in_g, global_hist, img_in.segment_size_g, img_size_g, 256);
+
+    // Prepare for MPI_Gatherv
+    int *sendcounts = (int *)malloc(size * sizeof(int));
+    int *displs = (int *)malloc(size * sizeof(int));
+
+    // Calculate send counts and displacements
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sendcounts[i] = (i < size - 1) ? (img_size_g / size) : (img_size_g / size + remainder_g);
+        displs[i] = sum;
+        sum += sendcounts[i];
+    }
+
+    // Gather the processed segments into the result image using MPI_Gatherv
+    MPI_Gatherv(enhanced_segment, img_in.segment_size_g, MPI_UNSIGNED_CHAR, result.img, sendcounts, displs, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+    // Clean up
+    free(enhanced_segment);
+    free(sendcounts);
+    free(displs);
+
     return result;
 }
 
-PPM_IMG contrast_enhancement_c_rgb(PPM_IMG img_in)
-{
-    PPM_IMG result;
-    int hist[256];
-    
-    result.w = img_in.w;
-    result.h = img_in.h;
-    result.img_r = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
-    result.img_g = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
-    result.img_b = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
-    
-    histogram(hist, img_in.img_r, img_in.h * img_in.w, 256);
-    histogram_equalization(result.img_r,img_in.img_r,hist,result.w*result.h, 256);
-    histogram(hist, img_in.img_g, img_in.h * img_in.w, 256);
-    histogram_equalization(result.img_g,img_in.img_g,hist,result.w*result.h, 256);
-    histogram(hist, img_in.img_b, img_in.h * img_in.w, 256);
-    histogram_equalization(result.img_b,img_in.img_b,hist,result.w*result.h, 256);
+//PPM_IMG contrast_enhancement_c_rgb(PPM_IMG img_in)
+//{
+//    PPM_IMG result;
+//    int hist[256];
+//
+//    result.w = img_in.w;
+//    result.h = img_in.h;
+//    result.img_r = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
+//    result.img_g = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
+//    result.img_b = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
+//
+//    histogram(hist, img_in.img_r, img_in.h * img_in.w, 256);
+//    histogram_equalization(result.img_r,img_in.img_r,hist,result.w*result.h, 256);
+//    histogram(hist, img_in.img_g, img_in.h * img_in.w, 256);
+//    histogram_equalization(result.img_g,img_in.img_g,hist,result.w*result.h, 256);
+//    histogram(hist, img_in.img_b, img_in.h * img_in.w, 256);
+//    histogram_equalization(result.img_b,img_in.img_b,hist,result.w*result.h, 256);
+//
+//    return result;
+//}
 
-    return result;
-}
 
+//PPM_IMG contrast_enhancement_c_yuv(PPM_IMG img_in)
+//{
+//    YUV_IMG yuv_med;
+//    PPM_IMG result;
+//
+//    unsigned char * y_equ;
+//    int hist[256];
+//
+//    yuv_med = rgb2yuv(img_in);
+//    y_equ = (unsigned char *)malloc(yuv_med.h*yuv_med.w*sizeof(unsigned char));
+//
+//    histogram(hist, yuv_med.img_y, yuv_med.h * yuv_med.w, 256);
+//    histogram_equalization(y_equ,yuv_med.img_y,hist,yuv_med.h * yuv_med.w, 256);
+//
+//    free(yuv_med.img_y);
+//    yuv_med.img_y = y_equ;
+//
+//    result = yuv2rgb(yuv_med);
+//    free(yuv_med.img_y);
+//    free(yuv_med.img_u);
+//    free(yuv_med.img_v);
+//
+//    return result;
+//}
 
-PPM_IMG contrast_enhancement_c_yuv(PPM_IMG img_in)
-{
-    YUV_IMG yuv_med;
-    PPM_IMG result;
-    
-    unsigned char * y_equ;
-    int hist[256];
-    
-    yuv_med = rgb2yuv(img_in);
-    y_equ = (unsigned char *)malloc(yuv_med.h*yuv_med.w*sizeof(unsigned char));
-    
-    histogram(hist, yuv_med.img_y, yuv_med.h * yuv_med.w, 256);
-    histogram_equalization(y_equ,yuv_med.img_y,hist,yuv_med.h * yuv_med.w, 256);
-
-    free(yuv_med.img_y);
-    yuv_med.img_y = y_equ;
-    
-    result = yuv2rgb(yuv_med);
-    free(yuv_med.img_y);
-    free(yuv_med.img_u);
-    free(yuv_med.img_v);
-    
-    return result;
-}
-
-PPM_IMG contrast_enhancement_c_hsl(PPM_IMG img_in)
-{
-    HSL_IMG hsl_med;
-    PPM_IMG result;
-    
-    unsigned char * l_equ;
-    int hist[256];
-
-    hsl_med = rgb2hsl(img_in);
-    l_equ = (unsigned char *)malloc(hsl_med.height*hsl_med.width*sizeof(unsigned char));
-
-    histogram(hist, hsl_med.l, hsl_med.height * hsl_med.width, 256);
-    histogram_equalization(l_equ, hsl_med.l,hist,hsl_med.width*hsl_med.height, 256);
-    
-    free(hsl_med.l);
-    hsl_med.l = l_equ;
-
-    result = hsl2rgb(hsl_med);
-    free(hsl_med.h);
-    free(hsl_med.s);
-    free(hsl_med.l);
-    return result;
-}
+//PPM_IMG contrast_enhancement_c_hsl(PPM_IMG img_in)
+//{
+//    HSL_IMG hsl_med;
+//    PPM_IMG result;
+//
+//    unsigned char * l_equ;
+//    int hist[256];
+//
+//    hsl_med = rgb2hsl(img_in);
+//    l_equ = (unsigned char *)malloc(hsl_med.height*hsl_med.width*sizeof(unsigned char));
+//
+//    histogram(hist, hsl_med.l, hsl_med.height * hsl_med.width, 256);
+//    histogram_equalization(l_equ, hsl_med.l,hist,hsl_med.width*hsl_med.height, 256);
+//
+//    free(hsl_med.l);
+//    hsl_med.l = l_equ;
+//
+//    result = hsl2rgb(hsl_med);
+//    free(hsl_med.h);
+//    free(hsl_med.s);
+//    free(hsl_med.l);
+//    return result;
+//}
 
 
 //Convert RGB to HSL, assume R,G,B in [0, 255]
@@ -103,9 +145,9 @@ HSL_IMG rgb2hsl(PPM_IMG img_in)
     img_out.s = (float *)malloc(img_in.w * img_in.h * sizeof(float));
     img_out.l = (unsigned char *)malloc(img_in.w * img_in.h * sizeof(unsigned char));
 
-    #pragma omp parallel for private(i, H, S, L) shared(img_in, img_out)
+#pragma omp parallel for private(i, H, S, L) shared(img_in, img_out)
     for(i = 0; i < img_in.w*img_in.h; i ++){
-        
+
         float var_r = ( (float)img_in.img_r[i]/255 );//Convert RGB to [0,1]
         float var_g = ( (float)img_in.img_g[i]/255 );
         float var_b = ( (float)img_in.img_b[i]/255 );
@@ -114,12 +156,12 @@ HSL_IMG rgb2hsl(PPM_IMG img_in)
         float var_max = (var_r > var_g) ? var_r : var_g;
         var_max = (var_max > var_b) ? var_max : var_b;   //max. value of RGB
         float del_max = var_max - var_min;               //Delta RGB value
-        
+
         L = ( var_max + var_min ) / 2;
         if ( del_max == 0 )//This is a gray, no chroma...
         {
-            H = 0;         
-            S = 0;    
+            H = 0;
+            S = 0;
         }
         else                                    //Chromatic data...
         {
@@ -134,17 +176,17 @@ HSL_IMG rgb2hsl(PPM_IMG img_in)
             if( var_r == var_max ){
                 H = del_b - del_g;
             }
-            else{       
+            else{
                 if( var_g == var_max ){
                     H = (1.0/3.0) + del_r - del_b;
                 }
                 else{
-                        H = (2.0/3.0) + del_g - del_r;
-                }   
+                    H = (2.0/3.0) + del_g - del_r;
+                }
             }
-            
+
         }
-        
+
         if ( H < 0 )
             H += 1;
         if ( H > 1 )
@@ -154,7 +196,7 @@ HSL_IMG rgb2hsl(PPM_IMG img_in)
         img_out.s[i] = S;
         img_out.l[i] = (unsigned char)(L*255);
     }
-    
+
     return img_out;
 }
 
@@ -174,22 +216,22 @@ PPM_IMG hsl2rgb(HSL_IMG img_in)
 {
     int i;
     PPM_IMG result;
-    
+
     result.w = img_in.width;
     result.h = img_in.height;
     result.img_r = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
     result.img_g = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
     result.img_b = (unsigned char *)malloc(result.w * result.h * sizeof(unsigned char));
 
-    #pragma omp parallel for private(i) shared(img_in, result)
+#pragma omp parallel for private(i) shared(img_in, result)
     for(i = 0; i < img_in.width*img_in.height; i ++){
         float H = img_in.h[i];
         float S = img_in.s[i];
         float L = img_in.l[i]/255.0f;
         float var_1, var_2;
-        
+
         unsigned char r,g,b;
-        
+
         if ( S == 0 )
         {
             r = L * 255;
@@ -198,7 +240,7 @@ PPM_IMG hsl2rgb(HSL_IMG img_in)
         }
         else
         {
-            
+
             if ( L < 0.5 )
                 var_2 = L * ( 1 + S );
             else
@@ -224,28 +266,28 @@ YUV_IMG rgb2yuv(PPM_IMG img_in)
     int i;//, j;
     unsigned char r, g, b;
     unsigned char y, cb, cr;
-    
+
     img_out.w = img_in.w;
     img_out.h = img_in.h;
     img_out.img_y = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
     img_out.img_u = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
     img_out.img_v = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
 
-    #pragma omp parallel for private(r, g, b, y, cb, cr) shared(img_in, img_out)
+#pragma omp parallel for private(r, g, b, y, cb, cr) shared(img_in, img_out)
     for(i = 0; i < img_out.w*img_out.h; i ++){
         r = img_in.img_r[i];
         g = img_in.img_g[i];
         b = img_in.img_b[i];
-        
+
         y  = (unsigned char)( 0.299*r + 0.587*g +  0.114*b);
         cb = (unsigned char)(-0.169*r - 0.331*g +  0.499*b + 128);
         cr = (unsigned char)( 0.499*r - 0.418*g - 0.0813*b + 128);
-        
+
         img_out.img_y[i] = y;
         img_out.img_u[i] = cb;
         img_out.img_v[i] = cr;
     }
-    
+
     return img_out;
 }
 
@@ -266,20 +308,20 @@ PPM_IMG yuv2rgb(YUV_IMG img_in)
     int i;
     int  rt,gt,bt;
     int y, cb, cr;
-    
-    
+
+
     img_out.w = img_in.w;
     img_out.h = img_in.h;
     img_out.img_r = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
     img_out.img_g = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
     img_out.img_b = (unsigned char *)malloc(sizeof(unsigned char)*img_out.w*img_out.h);
 
-    #pragma omp parallel for private(y, cb, cr, rt, gt, bt) shared(img_in, img_out)
+#pragma omp parallel for private(y, cb, cr, rt, gt, bt) shared(img_in, img_out)
     for(i = 0; i < img_out.w*img_out.h; i ++){
         y  = (int)img_in.img_y[i];
         cb = (int)img_in.img_u[i] - 128;
         cr = (int)img_in.img_v[i] - 128;
-        
+
         rt  = (int)( y + 1.402*cr);
         gt  = (int)( y - 0.344*cb - 0.714*cr);
         bt  = (int)( y + 1.772*cb);
@@ -288,6 +330,6 @@ PPM_IMG yuv2rgb(YUV_IMG img_in)
         img_out.img_g[i] = clip_rgb(gt);
         img_out.img_b[i] = clip_rgb(bt);
     }
-    
+
     return img_out;
 }
